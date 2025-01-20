22d1ebe7b84f8c7602996db37e3e4633c9a62518
courtoi223 <courtoi223@unicaen.fr>
2025-01-20 01:12:36 +0100

    creation migration(note : sealed_quality)

---
+++ database/migrations/2025_01_19_234111_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name');
+            $table->string('item_description');
+            $table->string('item_language');
+            $table->string('item_serie');
+            $table->double('item_price', 15, 8);
+            $table->timestamp('item_acquired_date')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Clé étrangère vers la table users
+            $table->timestamps(); // created_at et updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
